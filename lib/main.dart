533b7628012d38ae352eb392d643f41f20a96bdc
mrami8 <kram20008@gmail.com>
2025-04-01 20:27:52 +0200

    main mal, mañana se reempieza esto

--- lib/main.dart
+++ lib/main.dart
@@ -1,70 +1,27 @@
-import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
+import 'screens/home_screen.dart';
 
-class BuscarPlantasPage extends StatefulWidget {
-  const BuscarPlantasPage({super.key});
-
-  @override
-  _BuscarPlantasPageState createState() => _BuscarPlantasPageState();
+void main() {
+  runApp(const PlantasApp());
 }
 
-class _BuscarPlantasPageState extends State<BuscarPlantasPage> {
-  final TextEditingController _searchController = TextEditingController();
-  List<dynamic> _resultados = [];
-
-  Future<void> _buscarPlantas(String query) async {
-    if (query.isEmpty) {
-      setState(() {
-        _resultados = [];
-      });
-      return;
-    }
-
-    final url = Uri.parse("https://localhost/leafy_api/buscar_plantas.php?query=$query");
-    final response = await http.get(url);
-
-    if (response.statusCode == 200) {
-      List<dynamic> plantas = json.decode(response.body);
-      setState(() {
-        _resultados = plantas;
-      });
-    } else {
-      print("Error en la petición: ${response.statusCode}");
-    }
-  }
+class PlantasApp extends StatelessWidget {
+  const PlantasApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("Buscar Plantas")),
-      body: Column(
-        children: [
-          Padding(
-            padding: EdgeInsets.all(8.0),
-            child: TextField(
-              controller: _searchController,
-              onChanged: _buscarPlantas,  // Llama a la función cada vez que cambia el texto
-              decoration: InputDecoration(
-                hintText: "Buscar planta...",
-                border: OutlineInputBorder(),
-              ),
-            ),
-          ),
-          Expanded(
-            child: ListView.builder(
-              itemCount: _resultados.length,
-              itemBuilder: (context, index) {
-                return ListTile(
-                  leading: Image.network(_resultados[index]["imagen_url"]),
-                  title: Text(_resultados[index]["nombre"]),
-                  subtitle: Text(_resultados[index]["descripcion"]),
-                );
-              },
-            ),
-          ),
-        ],
+    return MaterialApp(
+      title: 'Plantas App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.green,
+          primary: Colors.green,
+          secondary: Colors.lightGreen,
+        ),
+        useMaterial3: true,
       ),
+      home: const HomeScreen(),
     );
   }
 }
